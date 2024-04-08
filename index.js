ed19de8bd69faa6d93687faf90b9bbf91f47d4e7
Ying Cai <i@caiying.me>
2024-04-08 23:04:07 +0100

    feat: support socks proxy

--- index.js
+++ index.js
@@ -7,10 +7,32 @@ const { testInsertDocuments, printInsertReport } = require("./bulk-insert");
 const uri = process.env.MONGODB_URI;
 const testDB = process.env.MONGODB_TEST_DB || "test_db";
 const testCollection = process.env.MONGODB_TEST_COLLECTION || "test_collection";
-const sharedClient = new MongoClient(uri);
+
+const proxyHost = process.env.PROXY_HOST;
+const proxyPort = process.env.PROXY_PORT;
+const proxyUsername = process.env.PROXY_USERNAME;
+const proxyPassword = process.env.PROXY_PASSWORD;
+
+let socksOptions;
+if (proxyHost && proxyPort) {
+  socksOptions = {
+    proxyHost: proxyHost,
+    proxyPort: proxyPort,
+    proxyUsername: proxyUsername,
+    proxyPassword: proxyPassword
+  };
+}
+
+let sharedClient = new MongoClient(uri);
+if (socksOptions) {
+  sharedClient = new MongoClient(uri, socksOptions);
+}
 
 async function testConnectionToServer() {
-  const client = new MongoClient(uri);
+  let client = new MongoClient(uri);
+  if (socksOptions) {
+    client = new MongoClient(uri, socksOptions);
+  }
   await testConnection(client);
 }
 
